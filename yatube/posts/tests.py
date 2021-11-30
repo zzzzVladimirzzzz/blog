66c3d09ed48c93bdfa28f64b600f1985f862cc82
Владимир Поваров <povarov.vova99@gmail.com>
2021-11-30 21:24:00 +0300

    last commit

--- yatube/posts/tests.py
+++ yatube/posts/tests.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
-# from django.test import TestCase
->>>>>>> ef96f46c21ebba25284fa4fa945c62413f9e4da7
+
 
 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -61,8 +61,6 @@ DATABASES = {
 }
 
 
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
